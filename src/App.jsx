ac36474808bab5c534ec8e89d2a261c58bffeb79
Devang Thesiya <79912919+devangthesiya@users.noreply.github.com>
2023-10-30 19:28:05 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Player from './components/Player.jsx';
+import TimerChallange from './components/TimerChallange.jsx';
+
+function App() {
+  return (
+    <>
+      <Player />
+      <div id="challenges">
+        <TimerChallange title='easy' targetTime={1}/>
+        <TimerChallange title='Not easy' targetTime={5}/>
+        <TimerChallange title='Tough' targetTime={10}/>
+        <TimerChallange title='Hard' targetTime={15}/>
+      </div>
+    </>
+  );
+}
+
+export default App;
